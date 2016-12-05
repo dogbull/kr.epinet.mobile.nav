a445de0fd6815d8a5e2c64d4f1f090e7ca0a9785
ParkJooHyeon <www.jrr.kr@gmail.com>
2016-12-05 17:02:35 +0900

    icon.psd 이동 및 splash 추가.

--- bin/makeicon.sh
+++ bin/makeicon.sh
@@ -3,10 +3,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-splashicon-generator --imagespath="${DIR}/../res/icons/"
+splashicon-generator --imagespath="${DIR}/../res/"
 
 rm -rf "${DIR}/../icon.png"
 rm -rf "${DIR}/../store"
 rm -rf "${DIR}/../res/icons/windows"
-rm -rf "${DIR}/../res/icons/wp8"
 rm -rf "${DIR}/../res/icons/android"
+rm -rf "${DIR}/../res/icons/wp8"
+
+rm -rf "${DIR}/../res/screens/windows"
+rm -rf "${DIR}/../res/screens/android"
+rm -rf "${DIR}/../res/screens/wp8"
+ 
